330fc7e7777df3adb09b00c0ad7e4e5cdc427799
cooljasonmelton <cooljasonmelton>
2020-07-03 02:11:26 -0500

    setup

---
+++ src/components/write/Notepad.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Notepad.css'
+
+class Notepad extends React.Component {
+    render(){
+
+        return (
+        <div className="Notepad">
+            What up
+
+        </div>
+    );
+}
+}
+
+export default Notepad;
