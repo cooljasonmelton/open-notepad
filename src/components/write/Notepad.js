e0267926e9a72a78a16afaefc8dbfa458315c022
cooljasonmelton <cooljasonmelton>
2020-07-03 02:29:09 -0500

    working out notepad css

--- src/components/write/Notepad.js
+++ src/components/write/Notepad.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './Notepad.css'
 
 class Notepad extends React.Component {
-    render(){
 
+    componentDidMount(){
+        this.nameInput.focus(); 
+     }
+
+
+    render(){
         return (
         <div className="Notepad">
-            What up
-
+            <textarea 
+            ref={(input) => { this.nameInput = input; }} 
+            className="text-area" />
+            <div className="info-box"> what up </div>
         </div>
     );
 }
